08d6a0c2478f266889b2a8b34caf2e8f710d46cf
Cyrille <clavigne@chem.utoronto.ca>
2023-06-24 22:41:20 -0400

    more composable

--- src/main/BaseFSM.scala
+++ src/main/BaseFSM.scala
@@ -6,17 +6,22 @@ import java.util.concurrent.CompletableFuture
 import java.util.concurrent.LinkedBlockingQueue
 import java.lang.InheritableThreadLocal
 
-abstract class BaseFSM[T <: BaseFSM[T]](f: T => Unit):
-  self: T =>
-
-  // Things to overload
+/** The base effect type is basically just a tuple with query and answer types. */
+trait Effect:
   type Q
   type A
 
+type Handler[E <: Effect] = (e: E) => e.Q => e.A
+
+abstract class Context[-E <: Effect]:
+  def suspend[E1 <: E](e: E1)(q: e.Q): CompletableFuture[e.A]
+
+abstract class BaseFSM[E <: Effect](f: Context[E] => Unit) extends Context[E]:
   // These always run before and after f, in the FSM's own thread. Use them to set handlers.
   def onEntry(): Unit = ()
   def onExit(): Unit = ()
 
+  // Queries
   // Messages from the FSM
   enum Message:
     def send(): Unit = messages.put(this)
@@ -25,7 +30,7 @@ abstract class BaseFSM[T <: BaseFSM[T]](f: T => Unit):
     case Init(thread: Thread)
 
     // answer the query by completing a to resume the FSM
-    case Request(q: Q, a: CompletableFuture[A])
+    case Query[E1 <: E](val e: E1)(val q: e.Q, val a: CompletableFuture[e.A])
 
     // FSM is done
     case Done
@@ -52,23 +57,22 @@ abstract class BaseFSM[T <: BaseFSM[T]](f: T => Unit):
       }
   Init(thread).send()
 
-  // Ask handler for a request.
-  def ask(q: Q): CompletableFuture[A] =
-    val answer = new CompletableFuture[A]
-    Request(q, answer).send()
-    answer
+  final override def suspend[E1 <: E](e: E1)(q: e.Q): CompletableFuture[e.A] =
+    val future = CompletableFuture[e.A]()
+    Query(e)(q, future).send()
+    future
 
   // ---------------------------------------------------------------------------------------------
   // Return the state of the FSM. Note that requests should *not* be lost if the FSM is to
   // proceed.
   def take(): Message = messages.take()
 
-  // Step through the FSM, answering any queries using the given function.
-  def join(f: Q => A): Unit =
+  // Step through the FSM with a given generic handler.
+  def join(handler: Handler[E]): Unit =
     while true do
       val state = take()
       state match
         case Init(t)              => t.start()
-        case Request(q, a)        => a.complete(f(q))
+        case q: Query[?]          => q.a.complete(handler(q.e)(q.q))
         case UncaughtException(e) => throw e
         case Done                 => return
--- src/test/BaseFSMTests.scala
+++ src/test/BaseFSMTests.scala
@@ -4,74 +4,82 @@ import java.util.concurrent.CompletableFuture
 import java.lang.InheritableThreadLocal
 
 object BaseFSMTests:
-  class UnitFSM(f: UnitFSM => Unit) extends BaseFSM[UnitFSM](f):
-    type Q = Unit
-    type A = Unit
+  object Async:
+    object Effect extends cola.Effect:
+      type Q = Unit
+      type A = Unit
 
-  class UppercaseFSM(f: UppercaseFSM => Unit) extends BaseFSM[UppercaseFSM](f):
-    type Q = String
-    type A = String
+    type Effect = Effect.type
+    val handler: Handler[Effect] = (_: Effect) => (_: Unit) => ()
+    class FSM(f: Context[Effect] => Unit) extends BaseFSM[Effect](f)
 
-  class StringProviderFSM(f: StringProviderFSM => Unit) extends BaseFSM[StringProviderFSM](f):
-    type Q = Unit
-    type A = String
+  object Str:
+    object Effect extends cola.Effect:
+      type Q = String
+      type A = String
+
+    type Effect = Effect.type
+    def handler(f: String => String): Handler[Effect] =
+      (_: Effect) => f
+    class FSM(f: Context[Effect] => Unit) extends BaseFSM[Effect](f)
 
 class BaseFSMTests extends munit.FunSuite:
   import BaseFSMTests.*
 
   test("join on FSM without pauses") {
     var result: String = ""
-    val fsm = new UnitFSM(_ => result = "ok")
-    fsm.join(identity)
+    val fsm = Async.FSM(_ => result = "ok")
+    fsm.join(Async.handler)
     assertEquals(result, "ok")
   }
 
   test("join on FSM with multiple pauses") {
     var result: String = ""
-    val fsm = new UnitFSM(
+    val fsm = Async.FSM(
       ctx =>
-        ctx.ask(())
+        ctx.suspend(Async.Effect)(()).get()
         result = "ok"
-        ctx.ask(())
+        ctx.suspend(Async.Effect)(()).get()
     )
-    fsm.join(identity)
+    fsm.join(Async.handler)
     assertEquals(result, "ok")
   }
 
   test("join on FSM with requests") {
     var result: String = ""
-    val fsm = new UppercaseFSM(
+    val fsm = new Str.FSM(
       ctx =>
-        val r1 = ctx.ask("hey").get()
-        val r2 = ctx.ask(" sup").get()
+        val r1 = ctx.suspend(Str.Effect)("hey").get()
+        val r2 = ctx.suspend(Str.Effect)(" sup").get()
         result = r1 + r2
     )
-    fsm.join((s: String) => s.toUpperCase)
+    fsm.join(Str.handler(_.toUpperCase))
     assertEquals(result, "HEY SUP")
   }
 
   test("join on FSM with multiple simultaneous requests") {
     var result: String = ""
-    val fsm = new UppercaseFSM(
+    val fsm = new Str.FSM(
       ctx =>
-        val requests = Seq("l", "o", "l").map(ctx.ask)
+        val requests = Seq("l", "o", "l").map(ctx.suspend(Str.Effect))
         val answers = requests.map(_.get())
         result = answers.mkString("")
     )
-    fsm.join((s: String) => s.toUpperCase)
+    fsm.join(Str.handler(_.toUpperCase))
     assertEquals(result, "LOL")
   }
 
   test("join on nested FSMs") {
     var result: String = ""
-    val fsm = new StringProviderFSM(
+
+    val fsm = new Str.FSM(
       provider =>
-        val fsm = new UppercaseFSM(
-          upper =>
-            val requests = (0 until 3).map(_ => provider.ask(()))
-            val toUpper = requests.map(_.thenCompose(upper.ask))
-            result = toUpper.map(_.get()).mkString(" ")
-        ).join((s: String) => s.toUpperCase)
-    ).join(Unit => "ok")
-    assertEquals(result, "OK OK OK")
+        val fsm = new Str.FSM(
+          uppercase =>
+            val requests = (0 until 3).map(_ => provider.suspend(Str.Effect)(""))
+            val upper = requests.map(_.thenCompose(uppercase.suspend(Str.Effect)))
+            result = upper.map(_.get()).mkString(" ")
+        ).join(Str.handler(_.toUpperCase))
+    ).join(Str.handler(_ => "lol"))
+    assertEquals(result, "LOL LOL LOL")
   }
